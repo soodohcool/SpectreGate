3849caee3071a2f267596b995f55c91dd6bcc161
soodohcool <59980933+soodohcool@users.noreply.github.com>
2020-08-18 11:09:07 -0700

    Update kennygscott_SpectreGate.lua
    add comments

--- kennygscott_SpectreGate.lua
+++ kennygscott_SpectreGate.lua
@@ -9,15 +9,22 @@
 --   **DISCLAIMER:** I am not affiliated with SpectreSoundStudios or Spectre Media Group nor do I claim to be!
 
 function F_ckYouGlenn()
+  -- Check if the user selected a track
   local track_selected = reaper.GetSelectedTrack(0, 0)
   if not track_selected then
     reaper.ShowMessageBox("Error: You must select a track before running this script!", "SpectreGate", 0)
   else
+    -- Duplicate the selected track
     reaper.Main_OnCommand(40062, 0);
+    -- Deselect all tracks
     reaper.Main_OnCommand(40289, 0);
+    -- Goto select the duplicated track
     reaper.Main_OnCommand(40287, 0);
+    -- Get track ID
     local id = reaper.GetSelectedTrack(0, 0);
+    -- Add ReaComp to the track
     reaper.TrackFX_AddByName(id, "ReaComp", false, 1)
+    -- Flip the phase of the track
     reaper.SetMediaTrackInfo_Value(id, "B_PHASE", 1)
   end
 end
